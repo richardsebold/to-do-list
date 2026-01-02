706791d139924eb78cef3fb8262bfddd3f1b8679
Richard Sebold <138056060+richardsebold@users.noreply.github.com>
2026-01-02 17:54:12 -0300

    feat: compleated static UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,111 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Plus, List, Check, Ban, SquarePen, Trash, ListCheck, Sigma } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
+
 export default function Home() {
- return (
-   <main>
-        <h1>Home</h1>
-   </main>
- );
+  return (
+    <main className="w-full h-screen bg-gray-100 flex items-center justify-center">
+      <Card className="w-lg">
+        <CardHeader className="flex gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button className="cursor-pointer"><Plus />Cadastrar</Button>
+        </CardHeader>
+
+        <CardContent>
+          <Separator className="mb-4" />
+
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer" variant={"default"}><List />Tarefas</Badge>
+            <Badge className="cursor-pointer" variant={"outline"}><Ban />Nao finalizadas</Badge>
+            <Badge className="cursor-pointer" variant={"outline"}><Check />Concluidas</Badge>
+          </div>
+
+
+          <div className="mt-4 border-b">
+
+            <div className="h-14 flex justify-between items-center border-t">
+              <div className="w-1 h-full bg-green-300"></div>
+              <p className="flex-1 px-2 text-sm">Estudar React</p>
+              <div className="flex gap-2 items-center">
+                <Dialog>
+
+                  <DialogTrigger asChild><SquarePen size={16} className="cursor-pointer" /></DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle >Editar tarefa</DialogTitle>
+                    </DialogHeader>
+
+                    <div className="flex gap-2">
+                      <Input placeholder="Editar tarefa" />
+                      <Button className="cursor-pointer">Editar</Button>
+                    </div>
+
+                  </DialogContent>
+                </Dialog>
+                <Trash size={16} className="cursor-pointer" />
+              </div>
+            </div>
+
+          </div>
+
+
+
+          <div className="flex justify-between mt-4">
+            <div className="flex gap-2 items-center">
+              <ListCheck size={18} />
+              <p className="text-xs">Tarefas concluidas</p>
+            </div>
+
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button className="cursor-pointer text-xs h-7" variant={"outline"}><Trash />Limpar tarefas concluidas</Button>
+              </AlertDialogTrigger>
+
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Tem certeza que deseja excluir X itens?</AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                  <AlertDialogAction>Continuar</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+
+          </div>
+
+          <div className="h-2 w-full bg-gray-100 mt-4 rounded-md">
+            <div className="h-full bg-blue-500 rounded-md" style={{ width: "50%" }}> </div>
+          </div>
+
+          <div className="flex justify-end items-center mt-2 gap-2">
+            <Sigma size={18} />
+            <p className="text-xs">Tarefas no total</p>
+          </div>
+
+
+
+        </CardContent>
+
+        <div></div>
+
+      </Card>
+    </main>
+  );
 }
